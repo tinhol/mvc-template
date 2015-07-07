ba5c2223a6e6fbb56fca7608a461db9f0ac02b15
degtyarjov <degtyarjov@haulmont.com>
2015-07-07 10:22:37 +0400

    'init'

---
+++ src/main/java/com/haulmont/mvc/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.haulmont.mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.haulmont.mvc")
+public class AppConfig {
+}
---
+++ src/main/java/com/haulmont/mvc/config/AppInitializer.java
@@ -0,0 +1,34 @@
+package com.haulmont.mvc.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * @author degtyarjov
+ * @version $Id$
+ */
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/*");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.haulmont.mvc.config");
+        return context;
+    }
+
+}
